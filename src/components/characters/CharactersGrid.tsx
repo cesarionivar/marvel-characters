ac570b95b04201d285c70fa04096ec0c45793b5e
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-29 19:05:51 -0400

    chore: move error and loading to components

--- src/components/characters/CharactersGrid.tsx
+++ src/components/characters/CharactersGrid.tsx
@@ -1,29 +1,17 @@
 import { useCharacters } from '../../hooks/useCharacters';
+import { Error, Loader } from '../ui';
 import { CharacterItem } from './CharacterItem';
 
 export const CharactersGrid = () => {
   const { characters, error, loading } = useCharacters();
 
-  if (loading) {
-    return (
-      <>
-        <h2 className='font-bold'>Loading...</h2>
-      </>
-    );
-  }
-
-  if (error) {
-    return (
-      <>
-        <h2 className='bg-red-800 text-white'>Error</h2>
-      </>
-    );
-  }
+  if (loading) return <Loader />;
+  if (error) return <Error />;
 
   return (
-    <div className='px-10 grid grid-cols-auto-fill mt-10 gap-6 dark:text-black'>
+    <div className='px-6 sm:px-20 grid grid-cols-auto-fill mt-10 mb-20 gap-6 dark:text-black'>
       {characters.map(character => (
-        <CharacterItem key={character.id} />
+        <CharacterItem key={character.id} character={character} />
       ))}
     </div>
   );
