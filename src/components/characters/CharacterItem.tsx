aeb7606f2ced799109c06290f7f58ae10be126ff
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-29 19:05:31 -0400

    chore: show character image

--- src/components/characters/CharacterItem.tsx
+++ src/components/characters/CharacterItem.tsx
@@ -1,3 +1,20 @@
-export const CharacterItem = () => {
-  return <div className='bg-white'>CharacterItem</div>;
+import { ICharacters } from '../../interfaces/charactersResponse';
+
+interface Props {
+  character: ICharacters;
+}
+
+export const CharacterItem = ({ character }: Props) => {
+  const { name, thumbnail } = character;
+
+  return (
+    <div className='dark:bg-slate-700 dark:text-white text-center rounded'>
+      <img
+        src={thumbnail.path + '.' + thumbnail.extension}
+        alt={name}
+        className='rounded-t h-60 object-cover object-left-top w-full'
+      />
+      <p className='my-3'>{name}</p>
+    </div>
+  );
 };
