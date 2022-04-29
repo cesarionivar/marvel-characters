f065325aab7d497b3b122899576dad3128e0a97b
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-29 19:06:01 -0400

    chore: add error component

---
+++ src/components/ui/Error.tsx
@@ -0,0 +1,9 @@
+export const Error = () => {
+  return (
+    <div className='m-20'>
+      <h2 className='bg-red-800 text-white rounded-md text-center text-lg'>
+        Error
+      </h2>
+    </div>
+  );
+};
