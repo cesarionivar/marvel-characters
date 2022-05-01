3d70009833c7f849fb40d46a257cf40b54d09fbd
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-30 21:40:03 -0400

    chore: move loader to characters grid

--- src/components/characters/CharactersGrid.tsx
+++ src/components/characters/CharactersGrid.tsx
@@ -5,17 +5,20 @@ import { CharacterItem } from './CharacterItem';
 export const CharactersGrid = () => {
   const { characters, error, loading } = useCharacters();
 
-  if (loading) return <Loader />;
   if (error) return <Error />;
 
   return (
     <div className='px-6 sm:px-20 my-10'>
       <Pagination />
-      <div className='grid grid-cols-auto-fill my-4 gap-6 dark:text-black'>
-        {characters.map(character => (
-          <CharacterItem key={character.id} character={character} />
-        ))}
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className='grid grid-cols-auto-fill my-4 gap-6 dark:text-black'>
+          {characters.map(character => (
+            <CharacterItem key={character.id} character={character} />
+          ))}
+        </div>
+      )}
       <Pagination />
     </div>
   );
