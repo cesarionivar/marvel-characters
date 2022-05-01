1707359b26d1c5792046b0691c87fffadcd51b71
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-05-01 18:38:07 -0400

    chore: add loadin spinner

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,7 +1,14 @@
 export const Loader = () => {
   return (
-    <div className='m-20 text-white min-h-[50vh] grid place-content-center'>
-      <h2 className='font-bold text-lg'>Loading...</h2>
+    <div className='m-20 min-h-[50vh] grid place-content-center'>
+      <div className='sk-chase'>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+        <div className='sk-chase-dot'></div>
+      </div>
     </div>
   );
 };
