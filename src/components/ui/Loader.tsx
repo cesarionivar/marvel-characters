e93f919517cd6a398a14fb04999f4417f594f7fc
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-30 21:40:14 -0400

    chore(style): center loading text

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <div className='m-20'>
+    <div className='m-20 text-white min-h-[50vh] grid place-content-center'>
       <h2 className='font-bold text-lg'>Loading...</h2>
     </div>
   );
