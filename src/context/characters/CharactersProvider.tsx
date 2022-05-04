0afc7256b6954e2feef4c6c1e7193a1ed5f61206
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-05-03 22:30:06 -0400

    chore: add variables to characters limit

--- src/context/characters/CharactersProvider.tsx
+++ src/context/characters/CharactersProvider.tsx
@@ -30,6 +30,9 @@ interface CharactersDataResponse {
   data: CharactersResponse;
 }
 
+const TEMP_TOTAL_RESULTS = 1562;
+const CHARACTERS_LIMIT = 20;
+
 export const CharactersProvider = ({ children }: Props) => {
   const [params, setParams] = useSearchParams();
   const isFirstRender = useRef<boolean>(true);
@@ -68,28 +71,34 @@ export const CharactersProvider = ({ children }: Props) => {
 
   const getCharactersByOffset = () => {
     let page: number = parseInt(params.get('page') ?? '1');
-    if (isNaN(page) || page < 1) {
+    if (
+      isNaN(page) ||
+      page < 1 ||
+      page > TEMP_TOTAL_RESULTS / CHARACTERS_LIMIT
+    ) {
       setParams({ page: '1' });
       page = 1;
     }
 
-    const offsetByPage = page * 20 - 20;
+    const offsetByPage = page * CHARACTERS_LIMIT - CHARACTERS_LIMIT;
 
     getCharacters(offsetByPage);
     dispatch({ type: 'set-offset', payload: offsetByPage });
   };
 
   const handlePreviousPage = () => {
-    const previous = offset <= 0 ? 0 : offset - 20;
-    const page = previous / 20 + 1;
+    const previous = offset <= 0 ? 0 : offset - CHARACTERS_LIMIT;
+    const page = previous / CHARACTERS_LIMIT + 1;
     setParams({ page: `${page}` });
     dispatch({ type: 'previous-page', payload: previous });
   };
 
   const handleNextPage = () => {
     const next =
-      offset >= totalCharacters ? totalCharacters - offset : offset + 20;
-    const page = next / 20 + 1;
+      offset >= totalCharacters
+        ? totalCharacters - offset
+        : offset + CHARACTERS_LIMIT;
+    const page = next / CHARACTERS_LIMIT + 1;
     setParams({ page: `${page}` });
     dispatch({ type: 'next-page', payload: next });
   };
