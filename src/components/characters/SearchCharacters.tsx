922b69d804f0d9345df1e32566de9c3257f35df0
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-05-09 00:52:39 -0400

    chore: start search characters feature

--- src/components/characters/SearchCharacters.tsx
+++ src/components/characters/SearchCharacters.tsx
@@ -1,4 +1,19 @@
+import { useState } from 'react';
+
 export const SearchCharacters = () => {
+  const [form, setForm] = useState({
+    searchTerm: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+
+    console.log(form);
+  };
+
   return (
     <div className='text-slate-900 mb-5'>
       <form>
@@ -7,9 +22,10 @@ export const SearchCharacters = () => {
             Search by name:
           </label>
           <input
-            type='search'
-            name='search'
-            id='search'
+            type='searchTerm'
+            onChange={handleChange}
+            name='searchTerm'
+            id='searchTerm'
             placeholder='Spider Man'
             className='px-2 py-2 rounded'
           />
